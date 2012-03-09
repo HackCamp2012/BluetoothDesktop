b7a5edb8816ec0095750a6405d2434d98d143c36
Philipp <mail@philipphock.de>
2012-03-09 22:19:04 +0100

    back to goep

--- src/BTServer.java
+++ src/BTServer.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.security.GuardedObject;
 
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
@@ -12,31 +13,20 @@ import javax.obex.SessionNotifier;
 public class BTServer {
 	private static final String SERVICEUUID = "6ABC1C60693D11E1B86C0800200C9A66";
 	private boolean listening=true;
-	private boolean active = true;
+	
 	public BTServer(){
 		
 	}
 	
 	public void init() throws IOException {
 		LocalDevice.getLocalDevice().setDiscoverable(DiscoveryAgent.GIAC);
-		
-		L2CAPConnectionNotifier notifier = (L2CAPConnectionNotifier)Connector.open("btl2cap://localhost:" + SERVICEUUID + ";name=RemoteHCIServer");
-		while (active){
-			L2CAPConnection con = notifier.acceptAndOpen();
-			System.out.println("Connection Received");
-			while (listening) {
-	            if (con.ready()){
-	            	byte[] recv=new byte[1024];
-	                con.receive(recv);
-	                String s = new String(recv, 0, recv.length);
-	                System.out.println("Received from server: " + s.trim());
-	                
-	                listening = false;
-	            }
-	        }	
+		SessionNotifier serverConnection = (SessionNotifier) Connector.open("btgoep://localhost:" + SERVICEUUID + ";name=RemoteHCIServer");
+		int count = 0;
+		while (count < 2){
+			BTRequestHandler rh = new BTRequestHandler();
+			serverConnection.acceptAndOpen(rh);
+			System.out.println("Connection Received" + (++count));
 		}
-				
-	}
-	
+	}	
 	
 }
