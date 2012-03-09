eef95f5c614eda30982d47cdb396d867ae773cf6
Philipp <mail@philipphock.de>
2012-03-09 23:37:43 +0100

    rfcomm

--- src/BTServer.java
+++ src/BTServer.java
@@ -1,32 +1,69 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.security.GuardedObject;
 
 import javax.bluetooth.BluetoothStateException;
 import javax.bluetooth.DiscoveryAgent;
-import javax.bluetooth.L2CAPConnection;
-import javax.bluetooth.L2CAPConnectionNotifier;
+
 import javax.bluetooth.LocalDevice;
 import javax.microedition.io.Connector;
-import javax.obex.SessionNotifier;
+import javax.microedition.io.StreamConnection;
+import javax.microedition.io.StreamConnectionNotifier;
 
 
-public class BTServer {
+public class BTServer  implements Runnable{
 	private static final String SERVICEUUID = "6ABC1C60693D11E1B86C0800200C9A66";
 	private boolean listening=true;
-	
+	private boolean active=true;
+	private StreamConnection con;
 	public BTServer(){
 		
 	}
 	
 	public void init() throws IOException {
-		LocalDevice.getLocalDevice().setDiscoverable(DiscoveryAgent.GIAC);
-		SessionNotifier serverConnection = (SessionNotifier) Connector.open("btgoep://localhost:" + SERVICEUUID + ";name=RemoteHCIServer");
-		int count = 0;
-		while (count < 2){
-			BTRequestHandler rh = new BTRequestHandler();
-			serverConnection.acceptAndOpen(rh);
-			System.out.println("Connection Received" + (++count));
+		Thread t = new Thread(this);
+		t.start();
+		
+	}
+
+	@Override
+	public void run() {
+		try {
+			LocalDevice.getLocalDevice().setDiscoverable(DiscoveryAgent.GIAC);
+		
+			StreamConnectionNotifier service =  (StreamConnectionNotifier) Connector.open( "btspp://localhost:" + SERVICEUUID + ";name=RemoteHCIServer" );
+			
+			while (active){
+	            con = (StreamConnection) service.acceptAndOpen();
+	            InputStream is = con.openInputStream();
+	            System.out.println("Connection Received");
+	            this.send("hello from Server".getBytes());
+	            while (listening){
+	            	byte[] recv= new byte[1024];
+	            	is.read(recv);
+	            	String s = new String(recv, 0, recv.length);
+	            	System.out.println("rec: "+s);
+	            }
+				
+			}
+		} catch (BluetoothStateException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
+		
 	}	
-	
+
+	public void send(byte[] b) throws IOException{
+        OutputStream os = con.openOutputStream();
+        os.write(b);
+	}
 }
+
+
+
+
+
+
+
